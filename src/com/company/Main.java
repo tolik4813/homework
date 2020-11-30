0a56be0019c478058d46f18049f6949313dfdf68
tolik4813 <afikusovich@gmail.com>
2020-11-30 18:57:23 +0200

    hw1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int a;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ведіть чило ");
+        a = scanner.nextInt();
+        if (a%2==0){
+            System.out.println("парне");
+        }
+        else {
+            System.out.println("непарне");
+        }
     }
 }
